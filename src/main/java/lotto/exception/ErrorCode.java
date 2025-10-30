657ce65a66579a7047e284eec1c997f8a4a4fa30
Seung <ryoograce25@gmail.com>
2025-10-31 03:35:29 +0900

    feat(Exception): enum에 예외 코드와 메세지 추가

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -10,9 +10,10 @@ public enum ErrorCode {
     ERROR_OUT_OF_RANGE("[ERROR] 당첨번호와 보너스번호는 1~45 사이의 숫자만 가능합니다."),
     ERROR_WINNING_NUMBER_DUPLICATE("[ERROR] 당첨번호끼리 중복되는 숫자가 존재해선 안됩니다."),
     ERROR_BONUS_NUMBER_DUPLICATE("[ERROR] 당첨번호와 보너스번호는 중복되는 숫자가 존재해선 안됩니다."),
+    ERROR_LOTTO_NUMBER_DUPLICATE("[ERROR] 로또 번호에 중복된 숫자가 있습니다."),
     ERROR_AMOUNT_NOT_DIVISIBLE_BY_THOUSAND("[ERROR] 로또 구매 금액은 1000원 단위여야 합니다."),
     ERROR_WINNING_NUMBERS_NOT_SIX("[ERROR] 당첨 번호는 총 6개의 숫자이어야 합니다"),
-    LOTTO_NUMBERS_NOT_SIX("[ERROR] 로또 번호는 6개여야 합니다.");
+    ERROR_LOTTO_NUMBERS_NOT_SIX("[ERROR] 로또 번호는 6개여야 합니다.");
 
     private final String message;
 
