f3c6ebfd114e688c40ca0a9d05e1b3186f45f833
Seung <ryoograce25@gmail.com>
2025-11-03 23:55:57 +0900

    feat(Result): 당첨결과 반환
    결과 반환 및 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -37,42 +37,5 @@ public class LottoController {
         WinningStatistics winningStatistics = new WinningStatistics(matchResults);
         OutputView.PrintWinningRate(winningStatistics.WinningRate(lottoCount*1000));
 
-        // <등수, 몇명>
-        // matchResult 로 구해야함
-        // matchResult 를 하나 넣고, n등에 +1명 hash에서 "2등을 가져올 떄 없다면 0을 디폴트로 "
-        //
-        //
-
     }
 }
-
-//        LottoWinning lottoWinning = new LottoWinning(matchCount, );
-//         long totalPrize  = lottoWinning.getWinningAmount();
-
-        // matchCount를 호출해야함
-        // matchBonus함수를 호출해야함 이 함수는 로또 번호가 5개 맞는 경우 실행되어야함
-
-
-        // 가격 입력 받기
-        // 가격 입력 검증
-        // 로또 번호 입력받기
-        // 로또 번호 입력 검증
-        // 보너스 번호 입력받기
-        // 보너스 번호 입력 검증
-        // 보너스 번호가 로또 번호에 없는지 확인
-
-        // 로또 생성하기
-
-        // 몇 개 맞으면 몇 등이고, 상금이 얼마인지 들어있는 이넘
-        // 로또 등수 체크하기
-        // 상금 계산하기
-        // 상금 출력
-
-        // Factory로 purchaseAmount 만큼 로또 생성하기
-        //  각각의 생성한 로또를 한 lottos에 담기
-
-        // lottos에 각각의 로또 등수 구하기
-        // 등수만큼 상금 구하기
-        // 결과 반환하기
-
-
--- src/main/java/lotto/model/MatchParser.java
+++ src/main/java/lotto/model/MatchParser.java
@@ -25,23 +25,3 @@ public class MatchParser {
         return numbers.contains(bonus);
     }
 }
-
-// [3, 4,5,6]
-// [[3,True], [1, False], [5, True], [5, True]]
-// 여기에 있는 함수들은 진짜 일치하는 수, 보너스랑 일치하는지 여부만 리턴하고있음
-// 이 정보들을 하나로 묶어야 등수를 구할 수 있는 데이터가 됨
-//
-
-
-/*
-Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-Lotto lotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
-Lotto winningLotto = new Lotto(List.of(1, 3, 5, 7, 9, 11));
-int bonus = 6;
-
-[
-  {matchCount=3, bonusMatch=true},
-  {matchCount=3, bonusMatch=false}
-]
-
- */
